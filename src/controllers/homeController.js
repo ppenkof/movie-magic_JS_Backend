986a594034780121b9dc14b67a5dad86cf4845cb
ppenkof <overton@abv.bg>
2025-09-22 15:52:11 +0300

    Show no movies

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,7 +6,7 @@ const homeController = Router();
     homeController.get('/', (req, res) => {
         const movies = movieService.getAll();
 
-        res.render('home');
+        res.render('home',{movies});
     });
 
     homeController.get('/about', (req, res) => {
--- src/model/Movie.js
+++ src/model/Movie.js
@@ -4,7 +4,7 @@ const movies = [
         title: "Jungle Cuise",
        genre: "Adventure",
        decription: "Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.",
-       imagerl: "/img/jungle-cruise.jpeg",
+       imagerl: "https://lumiere-a.akamaihd.net/v1/images/p_junglecruise_21740_v2_bb7f0ae4.jpeg",
        director: "Jaume Collet-Serra",
         year: "2021",
        ratng: 6.6,
