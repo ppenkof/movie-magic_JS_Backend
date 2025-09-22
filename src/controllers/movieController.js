5e51d7677c3124f6111acf7e75c5f889f9616e71
ppenkof <overton@abv.bg>
2025-09-22 21:20:46 +0300

    Render all movie on seartch page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -21,7 +21,8 @@ movieController.get('/:movieId/details', (req, res) => {
 });
 
 movieController.get('/search', (req, res) => {
-    res.render('search');
+    const movies = movieService.getAll();
+    res.render('search', { movies });
 });
 
 export default movieController;
