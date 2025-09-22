0ff3430b5dc0b70d083904634e5cc0090f7ee1c7
ppenkof <overton@abv.bg>
2025-09-22 22:25:33 +0300

    Add rating temp solution

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -17,13 +17,17 @@ movieController.post('/create', (req, res) => {
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.getOne(movieId);
-    res.render('details', { movie });
+    //prepare view data - temp solution
+    const ratinigViewData = '&#x2605'.repeat(Math.trunc(movie.rating));
+
+    res.render('details', { movie, ratinig: ratinigViewData });
 });
 
 movieController.get('/search', (req, res) => {
     const filter = req.query;
     const movies = movieService.getAll(filter);
     console.log(req.query);
+
     res.render('search', { movies, filter, pageTitle: 'Search Movies' });
 });
 
