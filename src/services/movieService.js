61ba0439c8cf4a7515e4678952e756f3b2c03e88
ppenkof <overton@abv.bg>
2025-09-22 16:37:36 +0300

    Movie service create

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,13 @@
+import { create } from "express-handlebars";
 import Movie from "../model/Movie.js";
 
 export default {
     getAll(){
         return Movie.find();    
+    },
+    create(movieData){
+        // const movie = new Movie(movieData);
+        // return movie.save();
+        console.log(movieData);
     }
 };
