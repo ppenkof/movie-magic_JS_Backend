d3a3c05917a176eeaf842b865bb78fa6a33aabf0
ppenkof <overton@abv.bg>
2025-09-22 13:03:21 +0300

    Add express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import handbars from 'express-handlebars';
+
 
 const app = express();
+
+// Setup Handlebars as the view engine
+app.engine('hbs', handbars.engine({extname: '.hbs'}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home',{layout: false});
 });
 
+//Start Server
 app.listen(5000, () => console.log('Server running on http://localhost:5000'));    
