5da8d52cb65b59462366467d146038d5cf2dda11
ppenkof <overton@abv.bg>
2025-09-30 21:23:40 +0300

    Add cast funtionality

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -7,4 +7,10 @@ castController.get('/create', (req, res) => {
     res.render('casts/create', { pageTitle: 'Cast Page' });
 });
 
+castController.post('/create', async (req, res) => {
+    const castData = req.body;
+    const result = await castService.create(castData);
+    res.redirect('/');
+});
+
 export default castController;
