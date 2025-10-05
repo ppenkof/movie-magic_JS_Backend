9b26af87830ab8bd9d79001a2e1377798f4af3bf
ppenkof <overton@abv.bg>
2025-10-05 18:03:28 +0300

    Save user to DB

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import authService from "../services/authService.js";
 
 const authController = Router();
 
 authController.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+authController.post('/register', async (req, res) => {
+    const userData = req.body;  
+    await authService.register(userData);
+    res.redirect('/');
 });
 
 export default authController;
