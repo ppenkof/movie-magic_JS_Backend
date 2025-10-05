6bcd6bd55092afa3c97d0a3f51e9629ddfff35fa
ppenkof <overton@abv.bg>
2025-10-05 17:36:58 +0300

    Add authController

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import castController from './controllers/castController.js';
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
+import authController from './controllers/authController.js';
 import { Router } from 'express';
 
 const routes = Router(); 
@@ -8,6 +9,7 @@ const routes = Router();
 routes.use(homeController);
 routes.use('/movies', movieController);
 routes.use('/casts', castController);
+routes.use('/auth', authController);
 
 //Add not found page 404
 routes.get('/*splat', (req, res) => {
